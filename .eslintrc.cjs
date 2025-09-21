002758a6cf1b7eb3a84fcd28b29cb99ae76ca37e
Victor Williams <vrwilliams719@gmail.com>
2025-09-21 05:09:48 -0400

    feat: update environment example and ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+  ignorePatterns: [
+    'dist/',
+    'coverage/',
+    'node_modules/',
+    '*.js',
+    '*.cjs',
+    '*.mjs',
+    '*.d.ts',
+  ],
+};
